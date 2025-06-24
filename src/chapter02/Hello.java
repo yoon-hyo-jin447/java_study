550265c11fbfca8f9e31dfb458b0d8ad77ddf8f2
yoon hyo jin <dbsgywls447@naver.com>
2025-06-24 16:55:35 +0900

    정리.md 추가

---
+++ src/chapter02/Hello.java
@@ -0,0 +1,9 @@
+package chapter02;
+
+public class Hello {
+	//main 메소드
+	public static void main (String[] args) {
+		System.out.print("Hello world!!");
+		System.out.println("Hello~");
+	}
+}
--- src/chapter02/HelloJava.java
+++
@@ -1,8 +0,0 @@
-package chapter02;
-
-public class HelloJava {
-	//main 메소드 - 실행을 위한 필수 메소드
-	public static void main(String[] args) {
-		System.out.println("Welcome to JavaWorld");
-	}
-}
