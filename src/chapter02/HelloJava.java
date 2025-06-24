d9f8b75b37fa72f18d69bf00fa75364ab6d4cf8f
yoon hyo jin <dbsgywls447@naver.com>
2025-06-24 14:10:15 +0900

    Initial commit

--- src/chapter02/HelloJava.java
+++ src/chapter02/HelloJava.java
@@ -3,6 +3,6 @@ package chapter02;
 public class HelloJava {
 	//main 메소드 - 실행을 위한 필수 메소드
 	public static void main(String[] args) {
-		System.out.println("안녕하세요");
+		System.out.println("Welcome to JavaWorld");
 	}
 }
